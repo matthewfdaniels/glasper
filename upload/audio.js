0e90fe4592e1c3932366a99b516e4a2841e89267
Matt Daniels <matthew.f.daniels@gmail.com>
2016-06-08 21:21:58 -0400

    wed night

--- upload/audio.js
+++ upload/audio.js
@@ -1,58 +1,28 @@
+var source;
+var lastSource;
+var scratch;
+var bufferArray;
+var vizData;
 
 function playMusic(){
 
-  clearInterval(sampleRotate);
-  d3.select(".number-one-date-wrapper").style("opacity",1);
+  // var playNow2 = createSource(scratch);
+  // source2 = playNow2.source;
 
-  var playNow2 = createSource(scratch);
-  source2 = playNow2.source;
+  // if (!source2.start){
+  //   source2.start = source.noteOn;
+  // }
+  // source2.start(0);
 
-  if (!source2.start){
-    source2.start = source.noteOn;
-  }
-  source2.start(0);
+  // var delayTime = 100;
+  // playing = true;
 
-  var delayTime = 100;
-  playing = true;
-
-  tickDelay = setTimeout(function(){
-
-    if(muted == false){
-
-      for (i=2; i<20; i++){
-        var timeOne = d3.time.day.offset(dates[dates.length-1], 7);
-        var timeTwo = d3.time.day.offset(dates[dates.length-1], 7*i);
-        if(nestedDatesTwo[unParse(timeOne)]["track"] != nestedDatesTwo[unParse(timeTwo)]["track"]){
-          playLength = Math.max((i-1) * shiftDuration,shiftDuration);
-          break;
-        }
-      }
-
-      var currTime = context.currentTime;
-    //
-      var currSong;
-      var previewItemId = nestedDatesTwo[unParse(d3.time.day.offset(dates[dates.length-1], 7))]["preview"];
-      currSong = "https://p.scdn.co/mp3-preview/" + previewItemId
-      if(previewItemId == "manual"){
-        var hostUrl = document.location.origin;
-        var pathUrl = document.location.pathname.replace("index.html","");
-        var idSong = nestedDatesTwo[unParse(d3.time.day.offset(dates[dates.length-1], 7))]["track"];
-        currSong = hostUrl+pathUrl+"url/"+idSong + ".m4a"
-      }
-      startTime = currTime + 1;
-      loadSounds(currSong, playLength, startTime, "now");
-  //
-    }
-    transition = d3.select({}).transition()
-        .duration(shiftDuration)
-        .ease("linear");
-
-    tick();
-
-  }, delayTime);
-
+  var currTime = context.currentTime;
+  var currSong = ["audio/keys_loop.wav","https://p.scdn.co/mp3-preview/14253ebfa0cbf0282999222261ba39c79d34f9fc"];
+  var startTime = currTime + 1;
+  var playLength = 30000;
+  loadSounds(currSong, playLength, startTime, "now");
 }
-
 
 function BufferLoader(context, urlList, callback, playingLength, startingTime, thing) {
   this.context = context;
@@ -117,19 +87,6 @@ function loadScratch(url) {
   req.send();
 }
 
-function loadHit(url) {
-  var req = new XMLHttpRequest();
-  req.open("GET",url,true);
-  req.responseType = "arraybuffer";
-  req.onload = function() {
-      //decode the loaded data
-      context.decodeAudioData(req.response, function(buffer) {
-          hit = buffer;
-      });
-  };
-  req.send();
-}
-
 function play() {
     //create a source node from the buffer
     var src = context.createBufferSource();
@@ -142,29 +99,16 @@ function play() {
 
 window.AudioContext = window.AudioContext || window.webkitAudioContext;
 context = new AudioContext();
-loadScratch("url/i_wish.mp3");
-// loadHit("url/scratch.wav");
+
 
 function loadSounds(url,playingLength,startingTime,thing){
-  if(url == "https://p.scdn.co/mp3-preview/NULL"){
-    console.log("null song");
-  }
-  else{
-    var bufferLoader = new BufferLoader(
-      context,
-      [
-        url
-      ],
-      finishedLoading,playingLength,startingTime,thing
-      );
-
-    bufferLoader.load();
-  }
-
+  var bufferLoader = new BufferLoader(context,url,finishedLoading,playingLength,startingTime,thing);
+  bufferLoader.load();
 }
 
 function finishedLoading(bufferList,playingLength,startingTime,thing) {
-  playHelper(bufferList[0],playingLength,startingTime,thing);
+  bufferArray = bufferList;
+  // playHelper(bufferList[0],playingLength,startingTime,thing);
 };
 
 function createSource(buffer) {
@@ -185,16 +129,15 @@ function createSource(buffer) {
 function playHelper(bufferNow,playingLength,startingTime,thing) {
 
     var currTime = context.currentTime;
-
     lastSource = source;
-
     var playNow = createSource(bufferNow);
     source = playNow.source;
     source.loop = true;
     var gainNode = playNow.gainNode;
     var duration = playingLength/1000 + 2;
 
-    gainNode.gain.linearRampToValueAtTime(0, startingTime);
+    gainNode.gain.linearRampToValueAtTime(1, startingTime);
+    // gainNode.gain.linearRampToValueAtTime(0, startingTime);
     gainNode.gain.linearRampToValueAtTime(1, startingTime + 2);
 
     if (!source.start){
@@ -207,3 +150,49 @@ function playHelper(bufferNow,playingLength,startingTime,thing) {
     gainNode.gain.linearRampToValueAtTime(0, startingTime + duration);
 
 }
+
+playMusic();
+// loadScratch("https://p.scdn.co/mp3-preview/b868c11bc56b76f19b4db1f199e43e0e7c13c90d");
+
+d3.select("body").on("click",function(){
+
+  playHelper(bufferArray[0],"30000",context.currentTime,"now");
+
+  //
+  // var playNow2 = createSource(bufferArray[0]);
+  // source2 = playNow2.source;
+  //
+  // if (!source2.start){
+  //   source2.start = source.noteOn;
+  // }
+  // source2.start(0);
+
+  // var playNow2 = createSource(scratch);
+  // source2 = playNow2.source;
+  //
+  // if (!source2.start){
+  //   source2.start = source.noteOn;
+  // }
+  // source2.start(0);
+})
+;
+
+var noteArray = [];
+
+d3.json("viz/keys_midi.json", function(error, data) {
+    console.log(data); // this is your data
+    for (key in data){
+      for (note in data[key].notes){
+        noteArray.push(data[key].notes[note]);
+      }
+    }
+    noteArray = noteArray.sort(function(b,a) {
+      var o1 = +a.startTime;
+      var o2 = +b.startTime;
+
+      if (o1 > o2) return -1;
+      if (o1 < o2) return 1;
+      return 0;
+    })
+    console.log(noteArray);
+});
